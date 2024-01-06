ab57e34988daf6c2c8bb316bcca64b414d2dfe95
Mahafuj Chowdhury <md.mahafuj012@gmail.com>
2024-01-06 18:50:50 +0600

    feat: Implement registration and login pages. [Frontend]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,37 @@
-import { Fragment } from "react";
+import { Fragment, Suspense } from "react";
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LazyLoader from "./components/LazyLoader";
+import RegistrationPage from "./pages/RegistrationPage";
+import LoginPage from "./pages/LoginPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
 
   return (
     <Fragment>
-      <h2 className="text-xl font-bold">Hello.</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/register"
+            element={
+              <Suspense fallback={<LazyLoader />}>
+                <RegistrationPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Suspense fallback={<LazyLoader />}>
+                <LoginPage />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      {/* <ScreenLoader /> */}
+      <Toaster position="top-center" />
     </Fragment>
   )
 }
